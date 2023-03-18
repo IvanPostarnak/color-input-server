0e815a945fa2860ae268a3354d6c3dddcab1b6f9
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-03-18 05:31:14 +0300

    feat: add 'create-server' function

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
+const path = require('path');
+const createServer = require(path.resolve('src', 'create-server.js'));
+
+const SERVER = createServer();
---
+++ src/create-server.js
@@ -0,0 +1,10 @@
+module.exports = createServer;
+
+const http = require('http');
+
+function createServer() {
+  const SERVER = http.createServer((request, response) => {
+
+  });
+  return SERVER;
+}
