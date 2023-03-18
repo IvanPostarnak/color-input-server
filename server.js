cd970924c87d4788765a645d8168ca8a61b9e8e5
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-03-18 06:46:31 +0300

    feat: add 'create-server-logs-dir' function

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const dotenv = require('dotenv');
-dotenv.config();
+const createServer = require(path.resolve('src', 'create-server.js'));
+const writeToLogFile = require(path.resolve('src', 'server-log.js'));
 
+dotenv.config();
 const PORT = process.env.PORT;
 const HOST = process.env.hostname;
 console.log(`PORT: ${PORT}, HOST: ${HOST}`);
 
-const createServer = require(path.resolve('src', 'create-server.js'));
 const SERVER = createServer();
+writeToLogFile();
