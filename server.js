1ea284c9a002667ec6b55804722ee2e8f78e31df
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-03-18 05:36:10 +0300

    fix: move 'dotenv' to devDependencies and console.log process.env

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const path = require('path');
-const createServer = require(path.resolve('src', 'create-server.js'));
+const PORT = process.env.PORT;
+const HOST = process.env.hostname;
+console.log(`PORT: ${PORT}, HOST: ${HOST}`);
 
+const createServer = require(path.resolve('src', 'create-server.js'));
 const SERVER = createServer();
