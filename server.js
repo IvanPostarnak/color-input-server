95088b642cf5ef49579af666a0d688c27f9a7f97
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-03-23 05:41:14 +0300

    feat: add 1st version of server getting request

--- server.js
+++ server.js
@@ -19,3 +19,29 @@ writeLog(`PORT: ${process.env.PORT}, HOST: ${process.env.hostname}`, SERVER_JS);
 // creating server Object
 const SERVER = createServer();
 writeLog(`SERVER: ${SERVER}, ${JSON.stringify(SERVER)}`, SERVER_JS);
+
+// reqcting on the request
+SERVER.on('request', (request, response) => {
+  const { method, url, headers } = request;
+  const userAgent = headers['user-agent'];
+  console.log(`method : '${method}'`);
+  console.log(`url : '${url}'`);
+  console.log(`headers : ${headers}`);
+  console.log(`userAgent : ${userAgent}`);
+
+  let requestData = [];
+  request.on('data', (chunk) => {
+    requestData.push(chunk);
+  })
+  request.on('end', () => {
+    requestData = Buffer.concat(requestData).toString();
+  })
+  request.on('error', (error) => {
+    console.error(error);
+  })
+})
+
+// make SERVER listen to the port
+SERVER.listen(process.env.PORT, () => {
+  writeLog(`SERVER.listen(${process.env.PORT})`, SERVER_JS);
+});
